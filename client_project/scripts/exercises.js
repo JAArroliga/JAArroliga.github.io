a1e7a1dbde5a52970e9b5c5d5d8aa4591bb1ea5c
Javier Arroliga <jarrolig@charlotte.edu>
2025-05-03 15:52:23 -0400

    small update to exercises

--- client_project/scripts/exercises.js
+++ client_project/scripts/exercises.js
@@ -3,15 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Code is running");
 
     const content = {
-        1: {content: 
-            `<p class="test-paragraph">
+        1: `<div class="test-paragraph">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>`
-        },
-        2: {content: `<div>test</div>`}
+            </div>`,
+        2: {content: `<div>test</div>`},
+        3: {}
     };
     
     const mainContent= document.getElementById("main-content");
